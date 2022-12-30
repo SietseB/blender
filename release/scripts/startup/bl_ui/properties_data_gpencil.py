550b86c0a0796bd088c158fa340d0df13d409ce2
Sietse Brouwer <info@brouwerinbeeld.nl>
2022-12-30 11:34:33 +0100

    Fix: stroke dryness slider greyed out by mistake

--- release/scripts/startup/bl_ui/properties_data_gpencil.py
+++ release/scripts/startup/bl_ui/properties_data_gpencil.py
@@ -188,11 +188,11 @@ class DATA_PT_gpencil_layers(DataButtonsPanel, Panel):
                 
                 layout.separator()
                 col = layout.column()
-                col.enabled = gpl.stroke_dryness == 0 and no_texture_image
+                col.enabled = (gpl.stroke_dryness == 0 and no_texture_image)
                 col.prop(gpl, "stroke_wetness", slider=True)
                 col = layout.column()
                 col.separator(factor=-0.9)
-                col.enabled = gpl.stroke_dryness == 0 and no_texture_image
+                col.enabled = no_texture_image
                 col.prop(gpl, "stroke_dryness", slider=True)
 
                 layout.separator()
